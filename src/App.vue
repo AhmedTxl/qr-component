dee3196f1ef756b0ebef88e5d3721930c561994c
AhmedTx <63393122+AhmedTxl@users.noreply.github.com>
2024-04-13 11:46:09 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import QrComponent from './components/QrComponent.vue'
+</script>
+
+<template>
+  <main class="w-full h-full flex flex-col items-center justify-center">
+      <QrComponent />
+      <div class="font-medium text-center mt-16">
+        Challenge by <a class="transition-all font-semibold hover:text-xl hover:text-blue-500" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        <br>
+        Coded by <a class="transition-all font-semibold hover:text-xl hover:text-blue-500" href="https://github.com/ahmedtxl">AhmedTx</a>.
+      </div>
+  </main>
+</template>
---
+++ src/components/QrComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-white p-4 shadow-xl rounded-xl gap-6 w-80 h-[32rem] flex flex-col items-center justify-start">
+    <img class="rounded-xl" src="../assets/image-qr-code.png" alt="QR Image">
+    <div class="px-4 text-center gap-3 flex flex-col items-center justify-center">
+      <h1 class="font-bold text-stroke-h1 text-xl text-[#1F314F]">Improve your front-end skills by building projects</h1>
+      <p class="font-semibold text-[15px]">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.text-stroke-h1 {
+  -webkit-text-stroke: thin;
+}
+</style>
